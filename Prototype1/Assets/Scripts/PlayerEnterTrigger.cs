0e112f73f0aa55bbb737e3cc82dba9d3da7ed03a
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-01-27 16:33:25 -0600

    Add Prototype 1

---
+++ Prototype1/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//attach this to the player
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    //set this reference in the inspector
+    public Text textbox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("TriggerZone"))
+        {
+            textbox.text = "You Win!";
+        }
+    }
+}
