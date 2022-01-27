973652b60e08745efbab6063c5694364f8c508e0
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-01-27 16:51:41 -0600

    Finished Prototype 1

---
+++ Prototype1/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If game not over, show score
+        if (!gameOver)
+            textbox.text = "Score: " + score;
+        //win condition: 3 or more points
+        if(score>=3)
+        {
+            won = true;
+            gameOver = true;
+        }
+        if(gameOver)
+        {
+            if(won)
+            {
+                textbox.text = "You win!\nPress R to Try Again!";
+            }
+            else
+            {
+                textbox.text = "You Lose!\nPress R to Try Again!";
+            }
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+}
--- Prototype1/Assets/Scripts/TriggerZoneAddScore.cs
+++ Prototype1/Assets/Scripts/TriggerZoneAddScore.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//attach this to the player
-public class PlayerEnterTrigger : MonoBehaviour
+public class TriggerZoneAddScore : MonoBehaviour
 {
-
+    private bool triggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("TriggerZone"))
+        if(other.CompareTag("Player") && !triggered)
         {
+            triggered = true;
             ScoreManager.score++;
         }
     }
